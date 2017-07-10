b990cd9adc35484d49692b4bbed61d61e6c06c73
정문기 <jmk0629>
2017-07-10 15:01:17 +0900

    common menu들 모듈화 완료!

--- src/js/common.js
+++ src/js/common.js
@@ -2,6 +2,28 @@
  * Created by 1027 on 2017-07-06.
  */
 
+var menus = require('./model/menu');
+
+
+function initMenu2() {
+    var template = require('../template/seoul-menu.hbs');
+    var menu = $('.seoul-menus');
+
+    /!*menu.empty();*!/
+
+    for(var i=0; i<menus.length; i++){
+        var menuHtml =template(menus[i]);
+
+        menu.append(menuHtml);
+
+    }
+}
+
+
+
+initMenu2();
+
+
 
 $('.seoul-menus > li').on('mouseover' , function () {
    $(this).find('.seoul-sub-menu').show();
---
+++ src/js/model/menu.js
@@ -0,0 +1,43 @@
+module.exports = [{
+    name: 'ABOUT',
+    subMenu: [{
+        name: '인사말'
+    },{
+        name: '미술관소개',
+        subSub: [{
+            name: '·미술관소개'
+        },{
+            name: '·MI소개'
+        }]
+    },{
+        name: '시설안내',
+        subSub: [{
+            name: '·주요시설'
+        },{
+            name: '·부대시설'
+        }]
+    }]
+},{
+    name: 'VISIT',
+    subMenu: [{
+        name: '관람안내'
+    },{
+        name: '오시는길'
+    }]
+},{
+    name: 'EXHIBITION'
+},{
+    name: 'NOTICE'
+},{
+    name: 'BOARD',
+    board: 'board',
+    subMenu: [{
+        name: '언론보도'
+    },{
+        name: '자료실'
+    },{
+        name: '공연 및 이벤트'
+    }]
+}];
+    
+    
