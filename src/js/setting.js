e9a6b650c2c4d65581e8548466297b51c9dff975
jmk0629 <jmkjmk7410@naver.com>
2017-09-20 18:09:02 +0900

    file 이미지 저장 추가. 이미지 불러와서 화면의 뿌려줌.

--- src/js/setting.js
+++ src/js/setting.js
@@ -36,7 +36,7 @@ function init(manager) {
         $('#seoul-manager-info-check').attr('checked', true);
     }
     if (manager.detail.avatar) {
-        $('#seoul-setting-avatar-select').css('background-image', 'url('+manager.detail.avatar+')');
+        $('.seoul-setting-avatar-img').css('background-image', 'url('+manager.detail.avatar+')');
     }
 
     $('#seoul-setting-avatar-select').on('click', function () {
@@ -59,6 +59,70 @@ function init(manager) {
 
     });
 
-
-
 }
+
+$('.seoul-setting-cancel').on('click', function () {
+   history.back();
+});
+
+$('.seoul-setting-save').on('click', function () {
+   var manager = {
+       currentPw: $('#seoul-manager-cpw-input').val().trim(),
+       password: $('#seoul-manager-npw-input').val().trim(),
+       detail : {
+           name: $('#seoul-manager-name-input').val().trim(),
+           phone: $('#seoul-manager-phone-input').cleanVal().trim(),
+           info: $('#seoul-manager-info-check')[0].checked ? 'Y' : 'N'
+       }
+   };
+   // 벨리데이션
+   var npwc = $('#seoul-manager-npwc-input').val().trim();
+   // 셋중에 하나라도 있으면 값이 걸린다.
+    if (manager.currentPw || manager.password || npwc) {
+        // 현재비밀번호
+        if(!manager.currentPw) {
+            alert('현재 비밀번호 입력!');
+            $('#seoul-manager-cpw-input').focus();
+            return;
+        }// 새 비밀번호
+        else if(!manager.password) {
+            alert('새 비밀번호 입력!');
+            $('#seoul-manager-npw-input').focus();
+            return;
+        }
+        else if(!npwc) {
+            alert('새 비밀번호 확인 입력!');
+            $('#seoul-manager-npwc-input').focus();
+            return;
+        }
+        else if(manager.password !== npwc) {
+            alert('새 비밀번호 다름!');
+            $('#seoul-manager-npwc-input').focus();
+            return;
+        }
+    }
+    var formData = new FormData();
+    // JSON을 String으로 바꿔주는녀석.
+    formData.append('manager',JSON.stringify(manager));
+
+    var images = $('#seoul-setting-avatar-input')[0].files;
+    if (images.length > 0) {
+        formData.append('avatar', images[0]);
+    }
+
+    common.ajax({
+       url:'/api/manager/save',
+       method: 'POST',
+       data:formData,
+       contentType: false,
+       processData: false,
+       success: function (result) {
+           alert('정상적으로 저장되었다.');
+
+           location.reload();
+       }
+    });
+
+
+   /*console.log(manager);*/
+});
